b9959b4692f91e30d4d2c4f46eecc0acf24a181f
seven7979 <2063916711@qq.com>
2023-06-13 09:13:31 +0800

    IDEA thrid commit

--- src/GitPackage/git.java
+++ src/GitPackage/git.java
@@ -4,5 +4,7 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello.git");
         System.out.println("hello.git222");
+        System.out.println("hello.git333");
+
     }
 }
